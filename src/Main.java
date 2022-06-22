975e295413d5a364e8b0b8af92a34d712d699c68
Nikolaeva Ekaterina <katy-forever@mail.ru>
2022-06-23 00:05:26 +0500

    Formatirovanie

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
+
     public static void main(String[] args) {
-
-        int amout = 5000; // стоимость билета
-        int rubMili = 20; // количество рублей для одной бонусной милли
-
-        int mili = amout / rubMili; // количество бонусных милль
-        System.out.println("Начислино " + mili + " бонусных миль");
+        int amout = 100; // стоимость билета
+        int oneMile = 20; // количество рублей для одной бонусной милли
+        int miles = amout / oneMile; // количество бонусных милль
+        System.out.println("Начислино " + miles + " бонусных миль");
     }
 }
