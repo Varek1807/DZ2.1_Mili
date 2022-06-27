4a708a108add963ed393bf8423f6e5b378d7056b
Nikolaeva Ekaterina <katy-forever@mail.ru>
2022-06-27 23:30:17 +0500

    DZ3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
-
     public static void main(String[] args) {
-        int amout = 100; // стоимость билета
-        int oneMile = 20; // количество рублей для одной бонусной милли
-        int miles = amout / oneMile; // количество бонусных милль
-        System.out.println("Начислино " + miles + " бонусных миль");
+
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
